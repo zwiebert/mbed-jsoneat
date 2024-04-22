b36abefbc0341b4a73936ec3f8446622f4739476
zwiebert <bertw@online.de>
2024-04-22 19:38:26 +0200

    Stop using esp-idf test runner

--- components/jsoneat/test/test_crash.cc
+++ components/jsoneat/test/test_crash.cc
@@ -1,13 +1,10 @@
 #include <unity.h>
-#ifdef TEST_HOST
-#include <test_runner.h>
-#endif
 
 #include <jsmn/jsmn_iterate.hh>
 char json[] = R"({"json":{"auto":{"adapter.0":{"name":"Neutral","flags":{"exists":1,"neutral":1,"read_only":1},"temp":0.06,"wind":0.001,"humi":0.01,"clouds":0.01}}}})";
 char json3[] = R"({"json":{}})";
 
-bool test_td() {
+void test_td() {
   auto jsmn = Jsmn<64, char *>(json);
   TEST_ASSERT_FALSE(!jsmn);
   auto it = jsmn.begin();
@@ -18,14 +15,13 @@ TEST_ASSERT_FALSE(it.keyStartsWith("gso"));
 TEST_ASSERT_FALSE(!it);
 it.skip_key_and_value();
 TEST_ASSERT_TRUE(!it);
-return true;
-
 }
 
+int main() {
+  UNITY_BEGIN();
 
+  RUN_TEST(test_td);
 
-
-TEST_CASE("crash", "[jsmn]")
-{
-  test_td();
+  return UNITY_END();
 }
+
